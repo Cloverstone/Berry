4ec6f7c3973d6b5b452af468e40e2f48e2b99523
Adam Smallcomb <adamsmallcomb@portal.local>
2014-10-22 13:33:30 -0400

    Switch to compiled for editor

--- assets/js/app_editor.js
+++ assets/js/app_editor.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 		cb = new cobler({target: '#editor', types: ['form']});
-		cb.load(JSON.parse(Cookies.get('form')));
+		cb.load(JSON.parse((Cookies.get('form') || "{}")));
 		cb.on("change",function(){
 
 				$('.result').html("<pre>"+JSON.stringify(this.toJSON(), undefined, 2)+"</pre>");
