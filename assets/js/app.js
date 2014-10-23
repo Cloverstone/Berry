d0cbb649199e7eb5fe83fc51e6a4313727425c6b
Adam Smallcomb <adamsmallcomb@portal.local>
2014-10-23 16:48:18 -0400

    Sync Style

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 $('#basic').on('click',function() {
 	//editor.setValue('{\n\t"fields": {\n\t\t"Name": {"type": "text"}, \n\t\t"Salutation": {"type": "select", "choices": ["Hello", "Bye"]}\n\t}\n}');
-editor.setValue('{\n"fields": '+JSON.stringify([
+editor.setValue(JSON.stringify({"fields": [
   {
     "label": "Name",
     "type": "text",
@@ -60,15 +60,15 @@ editor.setValue('{\n"fields": '+JSON.stringify([
       "Other"
     ]
   }
-]
-, undefined, 2)+'\n}');
+]}
+, undefined, 2));
 });
 
 $('#conditional').on('click', function(){
 	//editor.setValue('{\n\t"fields": {\n\t\t"Name": {"type": "text"}, \n\t\t"Salutation": {"type": "select", "choices": ["Hello", "Bye"]},\n\t\t	"Reason": {"type": "textarea", "show": {"matches": {"name": "salutation", "value": "Hello"}}}  \n\t}\n}');
 
 
-editor.setValue('{\n"fields": '+JSON.stringify([
+editor.setValue(JSON.stringify({"fields": [
   {
     "label": "Name",
     "type": "text",
@@ -92,8 +92,8 @@ editor.setValue('{\n"fields": '+JSON.stringify([
     ]
   },
 	{"label": "Reason", "name": "reason", "type": "textarea", "show": {"matches": {"name": "candy", "value": "Chocolate"}}}  
-]
-, undefined, 2)+'\n}');
+]}
+, undefined, 2));
 
 
 
@@ -101,7 +101,7 @@ editor.setValue('{\n"fields": '+JSON.stringify([
 
 $('#duplicate').on('click', function(){
 	//editor.setValue('{\n\t"fields": {\n\t\t"Name": {"type": "text"}, \n\t\t"Salutation": {"type": "select", "choices": ["Hello", "Bye"]},\n\t\t	"Reason": {"type": "textarea", "show": {"matches": {"name": "salutation", "value": "Hello"}}},\t"fs_c":{"type": "fieldset", "legend": "Thing", "fields": {\n\t\t"fs": {"label":false, "type": "fieldset","max":2, "multiple": {"duplicate": true}, "toArray": true, "fields": {\n\t\t\t"Name2": {}\n\t\t}\n\t}}}  \n\t}\n}');
-editor.setValue('{\n"fields": '+JSON.stringify([
+editor.setValue(JSON.stringify({"fields": [
   {
     "label": "Name",
     "type": "text",
@@ -118,8 +118,8 @@ editor.setValue('{\n"fields": '+JSON.stringify([
 			"Candy Type": {}
 		}
   }}}
-]
-, undefined, 2)+'\n}');
+]}
+, undefined, 2));
 
 
 });
@@ -164,5 +164,5 @@ $('#builder').on('click',function() {
 				delete stuff[i].widgetType;
 			}
 //	editor.setValue('{\n\t"fields": {\n\t\t"Name": {"type": "text"}, \n\t\t"Salutation": {"type": "select", "choices": ["Hello", "Bye"]}\n\t}\n}');
-	editor.setValue('{\n"fields": '+JSON.stringify(stuff, undefined, 2)+'\n}');
+	editor.setValue(JSON.stringify({"fields": stuff}, undefined, 2));
 });
