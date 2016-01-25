e2cfa31dc4726b64ebd9ced476cc3ecd1e97bf8a
Adam Smallcomb <atsmallcomb@gmail.com>
2016-01-25 13:14:11 -0500

    Merge remote-tracking branch 'origin/gh-pages' into gh-pages
    # Conflicts:
    #	bin/bootstrap.full.berry.js
    #	src/themes/bootstrap/advanced/berry_image_picker_collection.mustache

--- build/make.sh
+++ build/make.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-hulk ./src/js/themes/bootstrap/*.mustache ./src/js/themes/bootstrap/advanced/berry_tabs_fieldset.mustache ./src/js/themes/bootstrap/render/berry_tabs.mustache ./src/js/themes/bootstrap/advanced/modal.mustache > ./bin/bootstrap.berry.js
-hulk ./src/js/themes/bootstrap/*.mustache ./src/js/themes/bootstrap/render/*.mustache ./src/js/themes/bootstrap/advanced/*.mustache > ./bin/bootstrap.full.berry.js
-minify ./src/js/core.berry.js ./src/js/field.berry.js  ./src/js/events.berry.js ./src/js/init.berry.js  ./src/js/duplicate.berry.js ./src/js/conditions.berry.js  ./src/js/validations.berry.js ./src/js/fields/*.js > ./bin/berry.min.js
-minify ./src/js/render/tabs.berry.js ./src/js/enhance/modal.berry.js ./bin/bootstrap.berry.js > ./bin/bootstrap.berry.min.js
-minify ./src/js/core.berry.js ./src/js/field.berry.js  ./src/js/events.berry.js ./src/js/init.berry.js ./src/js/duplicate.berry.js ./src/js/conditions.berry.js  ./src/js/validations.berry.js ./src/js/fields/*.js ./src/js/enhance/*.js ./src/js/advanced/*.js ./src/js/render/*.js > ./bin/full.berry.min.js
+hulk ./src/themes/bootstrap/*.mustache ./src/themes/bootstrap/advanced/berry_tabs_fieldset.mustache ./src/themes/bootstrap/render/berry_tabs.mustache ./src/themes/bootstrap/advanced/modal.mustache > ./bin/bootstrap.berry.js
+hulk ./src/themes/bootstrap/*.mustache ./src/themes/bootstrap/render/*.mustache ./src/themes/bootstrap/advanced/*.mustache > ./bin/bootstrap.full.berry.js
+minify ./src/core.berry.js ./src/field.berry.js  ./src/events.berry.js ./src/init.berry.js  ./src/duplicate.berry.js ./src/conditions.berry.js  ./src/validations.berry.js ./src/fields/*.js > ./bin/berry.min.js
+minify ./src/render/tabs.berry.js ./src/enhance/modal.berry.js ./bin/bootstrap.berry.js > ./bin/bootstrap.berry.min.js
+minify ./src/core.berry.js ./src/field.berry.js  ./src/events.berry.js ./src/init.berry.js ./src/duplicate.berry.js ./src/conditions.berry.js  ./src/validations.berry.js ./src/fields/*.js ./src/enhance/*.js ./src/advanced/*.js ./src/render/*.js > ./bin/full.berry.min.js
 
 cp ./bin/berry.min.js ./examples/assets/
 cp ./bin/bootstrap.berry.js ./examples/assets/
